cd444534db902ec9ac934fc3f9226d1917f53fae
Slava Anishchuk <appsbaker@mail.ru>
2024-02-26 11:14:16 +0200

    Added Spacings

---
+++ Sources/DesignSystem/Spacings.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Slava Anishchuk on 24.02.2024.
+//
+
+import Foundation
+
+public enum Spacings {
+    public static let halfOfStandard: CGFloat = 8
+    public static let standard: CGFloat = 16
+    public static let doubleStandard: CGFloat = 32
+}
